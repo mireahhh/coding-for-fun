e9d8e5036317d43f6eb20e7f9f26c693f05c80ce
mireahhh <mireahhh@yandex.ru>
2026-03-17 05:54:49 +0300

    strt strkt

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -1,28 +1,46 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 function createPages(template, filename, chunks) {
   return new HtmlWebpackPlugin({
     template: template,
     filename: filename,
-    chunks: chunks
-  })
+    chunks: chunks,
+  });
 }
 
 const htmlPages = [
-  createPages('./src/index.html', './index.html', ['index']),
-  createPages('./src/pages/articles.html', './pages/articles.html', ['index']),
-  createPages('./src/pages/tests.html', './pages/tests.html', ['index']),
-  createPages('./src/pages/dictionary.html', './pages/dictionary.html', [
-    'index'
-  ]),
-  createPages(
-    './src/pages/articles/plants.html',
-    './pages/articles/plants.html',
-    ['index']
-  ),
-  createPages('./src/pages/tests/test1.html', './pages/tests/test1.html', [
-    'index'
-  ])
-]
+  createPages("./src/index.html", "./index.html", ["index"]),
+  createPages("./src/pages/handbook.html", "./pages/handbook.html", ["index"]),
+  createPages("./src/pages/gallery.html", "./pages/gallery.html", ["index"]),
+  createPages("./src/pages/rules.html", "./pages/rules.html", ["index"]),
+  createPages("./src/pages/about.html", "./pages/about.html", ["index"]),
 
-module.exports = htmlPages
+  // Галерея ./works
+  createPages("./src/pages/works/work_1.html", "./pages/works/work_1.html", [
+    "index",
+  ]),
+
+  // Часть_1 Модуль_1 ./part_1/module_1
+  createPages(
+    "./src/pages/parts/part_1/module_1.html",
+    "./pages/parts/part_1/module_1.html",
+    ["index"],
+  ),
+  createPages(
+    "./src/pages/parts/part_1/module_1/tutorial_1.html",
+    "./pages/parts/part_1/module_1/tutorial_1.html",
+    ["index"],
+  ),
+  createPages(
+    "./src/pages/parts/part_1/module_1/tutorial_2.html",
+    "./pages/parts/part_1/module_1/tutorial_2.html",
+    ["index"],
+  ),
+  createPages(
+    "./src/pages/parts/part_1/module_1/tutorial_3.html",
+    "./pages/parts/part_1/module_1/tutorial_3.html",
+    ["index"],
+  ),
+];
+
+module.exports = htmlPages;
