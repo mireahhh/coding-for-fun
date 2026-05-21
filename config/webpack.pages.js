c9b45473a6c0cd025aa72b975d651ad93b536ee2
mireahhh <mireahhh@yandex.ru>
2026-05-21 04:09:27 +0300

    stranica 404 v html

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -22,6 +22,7 @@ const htmlPages = [
   createPages("./src/pages/work.html", "./pages/work.html", ["index", "work"]),
   createPages("./src/pages/about.html", "./pages/about.html", ["index"]),
   createPages("./src/pages/adding.html", "./pages/adding.html", ["index", "adding"]),
+  createPages("./src/pages/notFound.html", "./pages/notFound.html", ["index"]),
   createPages(
     "./src/pages/parts/part1/module1/tutorial0.html",
     "./pages/parts/part1/module1/tutorial0.html",
