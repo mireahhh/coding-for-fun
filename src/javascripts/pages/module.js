f0ea78552d9cbcb3ffd0d3735aadb5dc9c59341f
mireahhh <mireahhh@yandex.ru>
2026-05-28 18:42:23 +0300

    ne idet raduga

--- src/javascripts/pages/module.js
+++ src/javascripts/pages/module.js
@@ -222,7 +222,7 @@ function drawModuleMeta() {
     const tutorial = moduleTutorialsData[index];
     if (!tutorial) return;
 
-    const subtitle = card.querySelector(".A_ModuleTutorialSubtitle");
+    const subtitle = card.querySelector(".W_ModuleTutorialSubtitle");
     const title = card.querySelector(".A_ModuleTutorialTitle");
     const tagsContainer = card.querySelector(".C_ModuleTutorialTags");
 
