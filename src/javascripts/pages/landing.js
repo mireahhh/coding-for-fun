b478dcf65b88e3fe1127ce4abec66b18bc4fe3a6
mireahhh <mireahhh@yandex.ru>
2026-05-31 10:56:59 +0300

    arrs landind karysel

--- src/javascripts/pages/landing.js
+++ src/javascripts/pages/landing.js
@@ -1,5 +1,17 @@
 import { tags } from "../json/otherJson.js";
 
+// Массивы для каруселей
+// 1я
+import history0 from "../../images/pages/landing/history0.png";
+import history1 from "../../images/pages/landing/history1.png";
+import history2 from "../../images/pages/landing/history2.png";
+// еще надо в массив запихнуть
+// 2я
+import book0 from "../../images/pages/landing/book0.png";
+import book1 from "../../images/pages/landing/book1.png";
+import book2 from "../../images/pages/landing/book2.png";
+// еще надо в массив запихнуть
+
 function createTagItem(tag) {
     const li = document.createElement("li");
     li.className = "A_LandingCoverRunningTag";
